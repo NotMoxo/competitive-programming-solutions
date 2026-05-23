7c2ff4de3fa0deeb81ef6a8ff45b35fff54a0466
NotMoxo <msdm0645@gmail.com>
2026-05-23 12:57:49 +0300

    Codeforces KCPC24 is it increasing(i) solution

---
+++ Codeforces/KCPC24_I_is_it_increasing.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LOOP(i, x, n) for (int i = x; i < n; i++)
+#define RLOOP(i, x, n) for (int i = x; i >= n; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define MOXO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define F first
+#define S second
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+#define int long long
+#define ull unsigned long long
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+/*
+                                 ███    ███  ██████   █   █   ██████
+                                 ████  ████ ██    ██   █ █   ██    ██
+                                 ██ ████ ██ ██ ██ ██    █    ██ ██ ██
+                                 ██  ██  ██ ██    ██   █ █   ██    ██
+                                 ██      ██  ██████   █   █   ██████
+*/
+
+void solve() {
+    int x1,y1,x2,y2; cin >> x1 >> y1 >> x2 >> y2;
+    cout << ((y2 - y1) * (x2 - x1) > 0 ? "YES" : "NO") << endl;
+}
+
+signed main() {
+    file();
+    MOXO
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+}
