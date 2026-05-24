523b139815eaa4acfecb53894a307294f61077eb
NotMoxo <msdm0645@gmail.com>
2026-05-24 06:36:28 +0300

    2229A Slimes On a line solution

---
+++ Codeforces/2229A Slimes On a line.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LOOP(i, x, n) for (int i = x; i < n; i++)
+#define RLOOP(i, x, n) for (int i = x; i >= n; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define MOXO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define F first
+#define S second
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+#define int long long
+#define ull unsigned long long
+
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+/*
+                                 ███    ███  ██████   █   █   ██████
+                                 ████  ████ ██    ██   █ █   ██    ██
+                                 ██ ████ ██ ██ ██ ██    █    ██ ██ ██
+                                 ██  ██  ██ ██    ██   █ █   ██    ██
+                                 ██      ██  ██████   █   █   ██████
+*/
+
+void solve() {
+    int n; cin >> n;
+    vector<int>v(n);
+    for (int i=0;i<n;i++) cin >> v[i];
+    int mn = *min_element(v.begin(), v.end()), mx = *max_element(v.begin(), v.end());
+    cout << (mx - mn + 1) / 2 << endl;
+}
+
+signed main() {
+    file();
+    MOXO
+    int T = 1;
+    cin >> T;
+    while (T--) solve();
+}
