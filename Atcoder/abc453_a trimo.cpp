52ff793c9839470921edf699c8696d1bd8bd1ee8
NotMoxo <msdm0645@gmail.com>
2026-05-26 10:46:17 +0300

    abc453 ab solutions

---
+++ Atcoder/abc453_a trimo.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LOOP(i, x, n) for (int i = x; i < n; i++)
+#define RLOOP(i, x, n) for (int i = x; i >= n; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define MOXO ios::sync_with_stdio(false); cin.tie(nullptr);
+#define F first
+#define S second
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+#define int long long
+#define ull unsigned long long
+
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+/*
+                                 ███    ███  ██████   █   █   ██████
+                                 ████  ████ ██    ██   █ █   ██    ██
+                                 ██ ████ ██ ██ ██ ██    █    ██ ██ ██
+                                 ██  ██  ██ ██    ██   █ █   ██    ██
+                                 ██      ██  ██████   █   █   ██████
+*/
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+    while (!s.empty() && s.front() == 'o' && n) {
+        s.erase(s.begin());
+        n--;
+    }
+    cout << s << endl;
+}
+signed main() {
+    file();
+    MOXO
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+}
